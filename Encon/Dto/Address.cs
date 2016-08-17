53755ea42afac975a97353d5f4f0bb6998334efa
Cory Cogdill <ccogdill@msn.com>
2016-08-17 10:40:52 -0500

    m2

--- Encon/Dto/Address.cs
+++ Encon/Dto/Address.cs
@@ -8,6 +8,12 @@ namespace Encon.Dto
 {
     public class Address
     {
+        // This is wrong, it needs an id of sorts.
+        //   I wonder if we have it all wrong, addresses really don't change, but the people and businesses that reside at those addresses do change.  
+        //   A person or business can also have multiple addresses.
+        //   Just as an address can have multiple phones.  
+
+        public int AddressID { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string Address3 { get; set; }
